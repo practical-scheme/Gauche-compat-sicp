1f35c4aec85cf03117cb38c2a0f369daf635bdee
Shiro Kawai <shiro@acm.org>
2017-02-11 14:01:27 -1000

    Remove unnecessary template test

--- test.scm
+++ test.scm
@@ -8,13 +8,6 @@
 (use compat.sicp)
 (test-module 'compat.sicp)
 
-;; The following is a dummy test code.
-;; Replace it for your tests.
-(test* "test-compat_sicp" "compat_sicp is working"
-       (test-compat_sicp))
-
-;; If you don't want `gosh' to exit with nonzero status even if
-;; the test fails, pass #f to :exit-on-failure.
 (test-end :exit-on-failure #t)
 
 
